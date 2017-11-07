47e244cd7a13dc26e72a1c8d932b983cce12d87d
Manoj Kumar <erpatel@gmail.com>
2017-11-06 19:31:33 -0800

    lambda

--- AWSLambdaWorkflow.Tests/FunctionTest.cs
+++ AWSLambdaWorkflow.Tests/FunctionTest.cs
@@ -14,16 +14,16 @@ namespace AWSLambdaWorkflow.Tests
     public class FunctionTest
     {
         [Fact]
-        public void TestToUpperFunction()
+        public async void TestToUpperFunctionAsync()
         {
 
             // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
             //var upperCase = function.FunctionHandler("hello world", context);
-            var upperCase = function.FunctionHandler( context);
+            var upperCase = await function.FunctionHandlerAsync( context);
 
-            Assert.Equal("HELLO WORLD", upperCase);
+            Assert.Equal("WaitingForActivation", upperCase);
         }
     }
 }
--- AWSLambdaWorkflow/Function.cs
+++ AWSLambdaWorkflow/Function.cs
@@ -1,5 +1,11 @@
 using Amazon.Lambda.Core;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using Amazon.StepFunctions;
+using Amazon.StepFunctions.Model;
 using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 //using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
@@ -11,25 +17,150 @@ namespace AWSLambdaWorkflow
 {
     public class Function
     {
-        
-        /// <summary>
+        //const aws = require('aws-sdk');
+               /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler( ILambdaContext context)
+        public async System.Threading.Tasks.Task<string> FunctionHandlerAsync(ILambdaContext context)
         {
-            //string input,
-            string input = "www";
-            var bucketName = System.Environment.GetEnvironmentVariable("mysqlconnection");
-            var quotenumber = GetQuote(58);
-            input = quotenumber.QuoteNumber;
-            return input?.ToUpper();
+            //try
+            //{
+            //    var bucketName = System.Environment.GetEnvironmentVariable("mysqlconnection");
+            //    var quotenumber = GetQuote(58);
+            //    string input = quotenumber.QuoteNumber;
+            //    return input?.ToUpper();
+            //}
+            //catch (Exception e)
+            //{ }
+
+            //Starts workflow instance with parameter
+            StartWorkflowInstance();
+
+            AmazonStepFunctionsClient stepfunctions = new AmazonStepFunctionsClient(Amazon.RegionEndpoint.USEast1);
+            AmazonSimpleEmailServiceClient ses = new AmazonSimpleEmailServiceClient(Amazon.RegionEndpoint.USEast1);
+
+            //return "Success";
+            //arn:aws:states:us-east-1:494875521123:stateMachine:PromotionApprovalStepFunction
+            GetActivityTaskRequest request = new GetActivityTaskRequest { ActivityArn= "arn:aws:states:us-east-1:494875521123:activity:PricingManagerStep" };
+            GetActivityTaskResponse response = await stepfunctions.GetActivityTaskAsync(request);
+            LambdaLogger.Log(response.Input);
+            string emailstatus = "Problem occured in email approval."; 
+            
+            if (!string.IsNullOrEmpty(response.Input))
+            {
+                //TODO get email from db
+                var input = JObject.Parse(response.Input);
+                var email = "manojsjsu@gmail.com"; //get to mail from salesOrg as per approval level
+                var quoteid = (int)input.SelectToken("quoteid");
+                var tolst = new List<string>();
+                tolst.Add(email);
+                Body body = new Body
+                {
+                    Html = new Content
+                    {
+                        Charset = "UTF-8",
+                        Data = "<div>Hi!<br /> " +
+                                    (string)input.SelectToken("promodesc") + " promotion need your approval!<br />" +
+                                    "Can you please approve:<br />" +
+                                    "https://dwjtuavq6g.execute-api.us-east-1.amazonaws.com/respond/approved?taskToken=" + Uri.EscapeUriString(response.TaskToken) + "<br />" +
+                                    "Or reject:<br />" +
+                                    "https://dwjtuavq6g.execute-api.us-east-1.amazonaws.com/respond/reject?taskToken=" + Uri.EscapeUriString(response.TaskToken) + "</div>"
+                    }
+                };
+                Content subjectContent = new Content
+                {
+                    Charset = "UTF-8",
+                    Data = "Your Approval Needed for Promotion Id " + quoteid
+                };
+                //return response.Input ?? "".ToString();
+                //body = new Body
+                //{
+                //    Html = new Content
+                //    {
+                //        Charset = "UTF-8",
+                //        Data = "<h1>Hi Test</h1>"
+                //    }//,
+                //    //Text = new Content
+                //    //{
+                //    //    Charset = "UTF-8",
+                //    //    Data = textBody
+                //    //}
+                //};
+
+
+                Message msg = new Message { Body = body, Subject = subjectContent };
+                SendEmailRequest s = new SendEmailRequest {
+                    Source = "erpatel@gmail.com",
+                    Destination= new Destination { ToAddresses= tolst },
+                    Message= msg, 
+                    ConfigurationSetName="sesconfigset",
+                    SourceArn= "arn:aws:ses:us-east-1:494875521123:identity/erpatel@gmail.com"
+                };
+                var sataus = ses.SendEmailAsync(s);
+                emailstatus =  sataus.Status.ToString();
+                //string input,
+                //var bucketName = System.Environment.GetEnvironmentVariable("mysqlconnection");
+                //var quotenumber = GetQuote(58);
+                //input = quotenumber.QuoteNumber;
+                //return input?.ToUpper();
+            }
+            return emailstatus;
         }
+
+        private void StartWorkflowInstance()
+        {
+            try {
+            List<Quote> quotes = GetQuotesAsPerStage(1);//1 for Submitted
+            foreach (var item in quotes)
+            {
+            //Check if any Promotion is submitted
+            string inputToStepFunc = "{ \"quoteid\": \""+ item.QuoteID + "\", \"promodesc\" : \""+ item.TPBackground + "\" }";
+
+            AmazonStepFunctionsClient stepfunctions = new AmazonStepFunctionsClient(Amazon.RegionEndpoint.USEast1);
+            StartExecutionRequest newRequest = new StartExecutionRequest { StateMachineArn = "arn:aws:states:us-east-1:494875521123:stateMachine:PromotionApprovalStepFunction", Input = inputToStepFunc };
+            stepfunctions.StartExecutionAsync(newRequest);
+            }
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+
+        private List<Quote> GetQuotesAsPerStage(int promoStage)
+        {
+            Quote quote = null;
+            List<Quote> lstquote = new List<Quote>();
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(string.Format("select * from Quote where QuoteStatusResultID ={0}", promoStage), conn);
+                
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        quote = new Quote()
+                        {
+                            QuoteID = Convert.ToInt32(reader["QuoteID"]),
+                            QuoteNumber = reader["QuoteNumber"].ToString(),
+                            QuoteTypeID = Convert.ToInt32(reader["QuoteTypeID"]),
+                            QuoteStatusResultID = Convert.ToInt32(reader["QuoteStatusResultID"]),
+                            TPBackground = reader["TPBackground"].ToString()
+                        };
+                        lstquote.Add(quote);
+                    }
+                }
+            }
+            return lstquote;
+        }
+
         private MySqlConnection GetConnection()
         {
-            return new MySqlConnection(System.Environment.GetEnvironmentVariable("mysqlconnection"));
+            string connectionstring = System.Environment.GetEnvironmentVariable("mysqlconnection");
+            
+            return new MySqlConnection(connectionstring);
         }
         public Quote GetQuote(int quoteid)
         {
